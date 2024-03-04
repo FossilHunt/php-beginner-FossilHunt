a170fe01e7800558e0acf06b5035e99a050afae8
FossilHunt <meinis2018@gmail.com>
2024-03-04 14:35:35 +0100

    hfst 3 bijna af

---
+++ 03-level/03-Rijles/script.php
@@ -0,0 +1,9 @@
+<?php
+
+$leeftijd =readline("Hoe oud ben je?". PHP_EOL);
+
+if($leeftijd == 17){
+    echo "Je mag beginnen met rijlessen!";
+} else{
+    echo "Je mag nog niet beginnen met rijlessen";
+}
