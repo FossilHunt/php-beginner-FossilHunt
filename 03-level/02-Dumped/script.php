76f5bdc56fcc5ec00bfcb5f9a4c7bdf7316baa4c
FossilHunt <meinis2018@gmail.com>
2024-04-15 13:11:11 +0200

    push

--- 03-level/02-Dumped/script.php
+++
@@ -1,6 +0,0 @@
-<?php
-$string = "Ik ben een string" . PHP_EOL;
-$nummer = 1594;
-
-var_dump($string);
-var_dump($nummer);
--- 03-level/03-Rijles/script.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$leeftijd =readline("Hoe oud ben je?". PHP_EOL);
-
-if($leeftijd == 17){
-    echo "Je mag beginnen met rijlessen!";
-} else{
-    echo "Je mag nog niet beginnen met rijlessen";
-}
